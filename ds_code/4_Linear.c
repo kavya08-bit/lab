f2fa9986d8325f7fcf6dba7275d3388df6a79a5d
Kavya Pal Singh <kavyapalsingh44@gmail.com>
2025-10-29 09:58:59 +0530

    Add files via upload

---
+++ ds_code/4_Linear.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int linearSearch(int arr[], int n, int target) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            return i;
+        }
+    }
+    return -1; 
+}
+int main() {
+    int arr[100];
+    int n, target, result;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    printf("Enter %d integers:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to search for: ");
+    scanf("%d", &target);
+
+    result = linearSearch(arr, n, target);
+
+    if (result == -1) {
+        printf("Element not found in the array.\n");
+    } else {
+        printf("Element found at position: %d\n", result);
+    }
+
+    return 0;
+}
---
+++ ds_code/5_Binary.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int binarySearch(int arr[], int n, int target) {
+    int low = 0;
+    int high = n - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == target) {
+            return mid; 
+        }
+        if (arr[mid] < target) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[100];
+    int n, target, result;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    printf("Enter %d integers IN SORTED ORDER:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to search for: ");
+    scanf("%d", &target);
+
+    result = binarySearch(arr, n, target);
+
+
+    if (result == -1) {
+        printf("Element not found in the array.\n");
+    } else {
+        printf("Element found at position: %d\n", result);
+    }
+
+    return 0;
+}
